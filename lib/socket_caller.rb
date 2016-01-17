6841836bc2dc8d7f8ecd6af19650074801198d45
danilo <danilopavkov@yahoo.com>
2016-01-18 00:00:50 +0100

    Pusher added for streaming to user

--- lib/socket_caller.rb
+++ lib/socket_caller.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
 require 'json'
 require "sinatra/json"
+require 'pusher'
+
+pusher_key = ENV['PUSHER_KEY']
+pusher_secret = ENV['PUSHER_SECRET']
+pusher_id = ENV['PUSHER_ID']
+Pusher.url = "https://#{pusher_key}:#{pusher_secret}@api.pusherapp.com/apps/#{pusher_id}"
+
 
 set :port, 4568
 
@@ -8,6 +15,7 @@ post '/do-stream', provides: 'json' do
   body = JSON.parse request.body.read
   validation_result = validate(body)
   if validation_result.nil?
+    stream_back(body['word'], body['color'], body['channel'])
     json body
   else
     status 400
@@ -15,13 +23,21 @@ post '/do-stream', provides: 'json' do
   end
 end
 
-
 def validate input
   if (! input["word"] || input["word"] == "")
     "no streaming without the word"
   elsif (! input["color"] || input["color"] == "")
     "no streaming without color"
+  elsif (! input["channel"] || input["channel"] == "")
+    "no streaming without channel, we wouldn't know where to stream"
   else
     nil
   end
 end
+
+def stream_back(word, color, channel)
+  Pusher.trigger(channel, 'word', {
+      word: word, color: color
+  })
+end
+
--- spec/socket_caller_spec.rb
+++ spec/socket_caller_spec.rb
@@ -2,24 +2,31 @@ require_relative '../lib/socket_caller'
 
 RSpec.describe "#validate" do
   context "with valid input" do
-    let (:input) { { "word" => "word", "color" => "black"} }
+    let (:input) { { "word" => "word", "color" => "black", "channel" => "channel"} }
     it "should return nil" do
       validation_result = validate(input)
       expect(validation_result).to eq nil
     end
   end
   context "without word" do
-    let (:input) { { "word" => "", "color" => "black"} }
+    let (:input) { { "word" => "", "color" => "black", "channel" => "channel"} }
     it "should return nil" do
       validation_result = validate(input)
       expect(validation_result).to eq "no streaming without the word"
     end
   end
   context "without color" do
-    let (:input) { { "word" => "word"} }
+    let (:input) { { "word" => "word", "channel" => "channel"} }
     it "should return nil" do
       validation_result = validate(input)
       expect(validation_result).to eq "no streaming without color"
+    end
+  end
+  context "without channel" do
+    let (:input) { { "word" => "word", "color" => "black"} }
+    it "should return nil" do
+      validation_result = validate(input)
+      expect(validation_result).to eq "no streaming without channel, we wouldn't know where to stream"
     end
   end
 end
